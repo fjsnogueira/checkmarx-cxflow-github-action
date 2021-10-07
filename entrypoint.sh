e924729f4d9bfe3dde338d4ad3795f2b2023bc10
Fernando Nogueira <fjsnogueira@gmail.com>
2021-10-07 16:25:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar /app/cx-flow.jar --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --cx-configuration="${CONFIGURATION}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
+java -jar /app/cx-flow.jar --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --cx-configuration="${CHECKMARX_CONFIGURATION}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
